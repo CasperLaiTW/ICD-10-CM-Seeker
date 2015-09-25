f59629bef4be7ae02eee94612ce9977fabb9fa29
Casper Lai <casper.lai@sleepingdesign.com>
2015-09-25 21:37:53 +0800

    Project initialize

---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const settings = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(true),
+    }),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+}
+
+export default settings;
