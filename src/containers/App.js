ebf95fedcbd862a35589a4c1749d62abcf7df6fd
Casper Lai <casper.lai@sleepingdesign.com>
2015-09-27 02:04:55 +0800

    Refactor app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import { Header } from '../components/';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
 export default class App extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
   render() {
     return (
       <div>
