77722104481572f0f70c66e47fbe359c92991ebe
Casper Lai <casper.lai@sleepingdesign.com>
2015-09-26 20:48:23 +0800

    Added home page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import App from './containers/App';
+import { Home } from './pages/';
 
 // Configure routes
 const routes = (
-  <Route path="/" component={App}>
+  <Route component={App}>
+    <Route path="/" component={Home} />
   </Route>
 );
 
