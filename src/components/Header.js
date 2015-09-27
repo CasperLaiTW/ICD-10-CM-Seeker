cb701250d6a17ec0ead4f12b3fa1e9fa5ae44f9f
Casper Lai <casper.lai@sleepingdesign.com>
2015-09-28 00:54:14 +0800

    Add GitHub link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 export default class Header extends React.Component {
   render() {
     return (
       <Navbar brand={<Link to="/">ICD-10-CM Seeker</Link>}>
+        <Nav right>
+          <NavItem href="https://github.com/CasperLaiTW/ICD-10-CM-Seeker">GitHub</NavItem>
+        </Nav>
       </Navbar>
     );
   }
