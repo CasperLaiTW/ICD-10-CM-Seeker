6e7f85a9cf85d91cb2500f2c3024f17cc79faf4e
Casper Lai <casper.lai@sleepingdesign.com>
2015-09-27 16:10:20 +0800

    Import ruby-sass-loader

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -9,6 +9,7 @@ export default {
   loaders: [
     { test: /\.js$/, loader: 'babel', query: { compact: false, stage: 0 } },
     { test: /\.css$/, loaders: ['style', 'css', 'cssnext'] },
+    { test: /\.scss$/, loaders: ['style', 'css', 'ruby-sass'] },
     { test: /\.svg$/, loader: "file-loader" },
     { test: /\.woff2?$/, loader: 'url-loader' },
     { test: /\.ttf$/, loader: "url-loader" },
