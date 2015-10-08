d306128322f707fbcc4ac716ea233e33e3aea322
Casper Lai <casper.lai@sleepingdesign.com>
2015-10-08 20:12:56 +0800

    Fix get root async

--- src/actions/RootActions.js
+++ src/actions/RootActions.js
@@ -2,8 +2,12 @@ import Core from 'icd-10-cm-core/dist/bundle';
 import * as RootConstants from '../constants/RootConstants';
 
 export function loadRoot() {
-  return {
-    type: RootConstants.DATA_LOADED,
-    root: Core.root
+  return (dispatch) => {
+    Core.getRoot().then(json => {
+      dispatch({
+        type: RootConstants.DATA_LOADED,
+        data: json,
+      });
+    });
   }
 }
