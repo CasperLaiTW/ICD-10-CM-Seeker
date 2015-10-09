b69abc9750a2cfd75a59ef3c8b206665815b2666
Casper Lai <casper.lai@sleepingdesign.com>
2015-10-09 19:08:19 +0800

    Hide panel on load

--- src/utils/devTools.js
+++ src/utils/devTools.js
@@ -10,7 +10,7 @@ export function renderDevTools(store) {
     const { DevTools, DebugPanel, LogMonitor } = require('redux-devtools/lib/react');
     return (
       <DebugPanel top right bottom>
-        <DevTools store={store} monitor={LogMonitor} />
+        <DevTools store={store} monitor={LogMonitor} visibleOnLoad={false} />
       </DebugPanel>
     );
   }
